5d4789456ce8ca7de221ff6f9b5df2ece9486f93
Kseniia Turusova <kseniya6665@gmail.com>
2025-12-28 13:26:20 +0100

    comment for the task_3 file has been fixed.

--- task_3.py
+++ task_3.py
@@ -1,27 +1,27 @@
 class PointsForPlace:
-    points = 0
 
     @staticmethod
     def get_points_for_place(place):
+        points = 0
         if place > 100:
              print('Баллы начисляются только первым 100 участникам')
-             return PointsForPlace.points
+             return points
         elif place < 1:
              print('Спортсмен не может занять нулевое или отрицательное место')
-             return PointsForPlace.points
+             return points
         else:
             points = 101 - place
             return points
 
 
 class PointsForMeters:
-    points = 0
 
     @staticmethod
     def get_points_for_meters(meters):
+        points = 0
         if meters < 0:
              print('Количество метров не может быть отрицательным')
-             return PointsForMeters.points
+             return points
         else:
             points = meters * 0.5
             return points
